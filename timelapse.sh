a0c630acff76fa1c75ed3b6326aa07ac0b4ce275
Adriel <adriel@users.noreply.github.com>
2016-05-24 14:31:36 +1200

    Test commit

--- timelapse.sh
+++ timelapse.sh
@@ -4,6 +4,7 @@ FFMPG_BIN='/Users/adriel/Downloads/TimeLapse-Creater-master/ffmpeg'
 
 # Directory input (no trailing slash needed)
 INPUT_DIR=$1
+
 # Quality of output (hd480,hd720,hd1080)
 QUALITY=$2
 FPS=$3
@@ -20,7 +21,7 @@ TMP_DIR=$TMPDIR'timelapse_tmp'
 rm -r $TMP_DIR
 
 if [[ -d "$INPUT_DIR" ]]; then
-	
+
 	# Create tmp dir with links
 	mkdir "$TMP_DIR"
 	echo 'Temp dir: '$TMP_DIR
@@ -34,13 +35,13 @@ if [[ -d "$INPUT_DIR" ]]; then
 		while read -r FILE; do
 		    # echo "... $line ..."
 			((COUNT++))
-			COUNT_FILL=$(printf "%04d" $COUNT) # Change to auto detect how long the no. should be 
+			COUNT_FILL=$(printf "%04d" $COUNT) # Change to auto detect how long the no. should be
 			FILE_EXT=$(echo $FILE | grep -E '\.([^\.]+)$' -o)
 			# echo $COUNT_FILL
 			# FNAME=$(basename "$FILE")
 			ln -s "$FILE" "$TMP_DIR/${COUNT_FILL}${FILE_EXT}"
 		done <<< "$IMAGES"
-		
+
 		echo 'Combine imgs '$FILE_EXT
 		# Create video
 		$FFMPG_BIN -i "$TMP_DIR/%04d$FILE_EXT" -r $FPS -s $QUALITY -vcodec libx264  -pix_fmt yuv420p -threads 0 "$INPUT_DIR"/timelapse1.mp4
@@ -48,7 +49,7 @@ if [[ -d "$INPUT_DIR" ]]; then
 	else
 		echo 'Temp dir could not be found'
 	fi
-	
+
 else
 	echo 'Folder not found.'
 fi
